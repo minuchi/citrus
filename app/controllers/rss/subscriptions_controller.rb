d52256bb81ac5483d1b614f78cccdfc81a18f2c2
minuchi <kimkimee2@gmail.com>
2025-01-12 12:41:21 +0900

    feat(rss): add CRUD for subscriptions

--- app/controllers/rss/subscriptions_controller.rb
+++ app/controllers/rss/subscriptions_controller.rb
@@ -6,17 +6,31 @@ class Rss::SubscriptionsController < ApplicationController
   end
 
   def new
+    @subscription = Rss::Subscription.new
+    @urls = Rss::Url.all
   end
 
   def create
+    @subscription = Rss::Subscription.new(rss_subscription_params.merge(user: @current_user))
+    if @subscription.save
+      redirect_to rss_subscriptions_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @subscription = Rss::Subscription.find(params[:id])
+    @subscription.destroy
+    redirect_to rss_subscriptions_path
   end
 
   private
     def set_current_user
       @current_user ||= Current.session.user if Current.session
+    end
+
+    def rss_subscription_params
+      params.expect([ rss_subscription: [ :rss_url_id ] ])
     end
 end

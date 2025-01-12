fa33374951467cc83f067ff596e9bc4a3afa4d29
minuchi <kimkimee2@gmail.com>
2025-01-12 12:57:21 +0900

    chore(rss): rename subscription to rss_subscription

--- app/controllers/rss/subscriptions_controller.rb
+++ app/controllers/rss/subscriptions_controller.rb
@@ -2,18 +2,18 @@ class Rss::SubscriptionsController < ApplicationController
   before_action :set_current_user
 
   def index
-    @subscriptions = @current_user.rss_subscriptions
+    @rss_subscriptions = @current_user.rss_subscriptions
   end
 
   def new
-    @subscription = Rss::Subscription.new
-    subscribed_url_ids = @current_user.rss_subscriptions.pluck(:rss_url_id)
-    @urls = Rss::Url.where.not(id: subscribed_url_ids)
+    @rss_subscription = Rss::Subscription.new
+    subscribed_rss_url_ids = @current_user.rss_subscriptions.pluck(:rss_url_id)
+    @urls = Rss::Url.where.not(id: subscribed_rss_url_ids)
   end
 
   def create
-    @subscription = Rss::Subscription.new(rss_subscription_params.merge(user: @current_user))
-    if @subscription.save
+    @rss_subscription = Rss::Subscription.new(rss_subscription_params.merge(user: @current_user))
+    if @rss_subscription.save
       redirect_to rss_subscriptions_path
     else
       render :new, status: :unprocessable_entity
@@ -21,8 +21,8 @@ class Rss::SubscriptionsController < ApplicationController
   end
 
   def destroy
-    @subscription = Rss::Subscription.find(params[:id])
-    @subscription.destroy
+    @rss_subscription = Rss::Subscription.find(params[:id])
+    @rss_subscription.destroy
     redirect_to rss_subscriptions_path
   end
 

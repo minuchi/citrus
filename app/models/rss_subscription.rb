d907d5cf468ed7596091497257f22104e309a2ff
minuchi <kimkimee2@gmail.com>
2025-01-19 13:28:11 +0900

    feat(rss): validate data for rss subscription

--- app/models/rss_subscription.rb
+++ app/models/rss_subscription.rb
@@ -1,4 +1,21 @@
 class RssSubscription < ApplicationRecord
   belongs_to :user
   belongs_to :rss_url
+
+  validates :user, presence: true
+  validates :rss_url, presence: true
+  validates :rss_url_id, uniqueness: {
+    scope: :user_id,
+    message: "is already subscribed by you"
+  }
+
+  validate :user_subscription_limit
+
+  private
+
+  def user_subscription_limit
+    if user.present? && user.rss_subscriptions.count >= 100
+      errors.add(:base, "You can't have more than 100 RSS subscriptions")
+    end
+  end
 end
